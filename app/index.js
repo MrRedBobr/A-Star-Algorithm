261355d81811fbaa323fbeeaa7583258c0d2de79
MrRedBobr <46565863+MrRedBobr@users.noreply.github.com>
2020-02-19 18:04:43 +0300

    Правочки

--- app/index.js
+++ app/index.js
@@ -1,53 +1,45 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-
-const isDev = process.env.NODE_ENV === 'development';
-const isProd = process.env.NODE_ENV === 'production';
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow = null;
-function createWindow () {
+function createWindow() {
 	// Create the browser window.
 	mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
-		webPreferences: {
-			nodeIntegration: true
-		}
+		width: 1024,
+		height: 650
 	});
 	mainWindow.removeMenu();
 
 	// and load the index.html of the app.
-	if(isDev) {
-		mainWindow.loadURL('http://localhost:3000/');
-		// Open the DevTools.
-		mainWindow.webContents.openDevTools();
-	} else mainWindow.loadFile('./app/dist/index.html');
+	// Open the DevTools.
+	mainWindow.webContents.openDevTools();
+	mainWindow.loadFile('./app/src/index.html');
 
 	// Emitted when the window is closed.
-	mainWindow.on('closed', function () {
+	mainWindow.on('closed', function() {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
-		mainWindow = null
-	})
+		mainWindow = null;
+	});
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
 	// On macOS it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') app.quit()
-})
+	if (process.platform !== 'darwin') app.quit();
+});
 
-app.on('activate', function () {
+app.on('activate', function() {
 	// On macOS it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) createWindow()
-})
+	if (mainWindow === null) createWindow();
+});
